facf9248c80ffd1d0256e263f6f14aa4bc73a3d0
delaw.ju <delaw.ju@gmail.com>
2022-03-01 22:57:13 +0900

    First commit
    Initialize Project

---
+++ src/test/java/tm/booup/tellme/TellmeApplicationTests.java
@@ -0,0 +1,13 @@
+package tm.booup.tellme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
